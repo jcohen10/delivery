0784e71d7747c89777b6cf94e4ef99131808bb9b
Joseph Cohen <jdcohen9@gmail.com>
2015-04-22 14:13:36 -0400

    add new pages view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
   end
+  def erb_demo
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
 
+  get 'pages/erb_demo'
+
 end
